e4272d9d2a9cf3d81fd003586922be2b681bf8e0
Izumu Mishima <izmishi11@gmail.com>
2016-07-13 01:17:40 +0100

    Initial Commit

---
+++ Script/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Script
+//
+//  Created by Izumu Mishima on 13/07/2016.
+//  Copyright © 2016 Izumu Mishima. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+
+		// Do any additional setup after loading the view.
+	}
+
+	override var representedObject: AnyObject? {
+		didSet {
+		// Update the view, if already loaded.
+		}
+	}
+
+
+}
+
