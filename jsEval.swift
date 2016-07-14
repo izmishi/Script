e5e1b3c94b49ac7158c841aeaa151189f2dea5ff
Izumu Mishima <izmishi11@gmail.com>
2016-07-14 11:51:43 +0100

    iOS version: 	fixed minimum font jsEval: 	better output

--- jsEval.swift
+++ jsEval.swift
@@ -9,19 +9,37 @@
 import Foundation
 import JavaScriptCore
 
-func jsEval(script: String, context: JSContext) -> (eval: JSValue, msg: String) {
-	let evaluated = context.evaluateScript(script)
-	var message: String = ""
-	if evaluated == JSValue.init(undefinedIn: context) {
+func getVariableName(script: String, context: JSContext) -> String? {
+	if script.contains("=") {
+		var str = script
 		if script.hasPrefix("var ") {
 			let index = script.index(script.startIndex, offsetBy: 4)
-			message = script[index..<script.endIndex]
-		} else {
-			if context.evaluateScript("Math.\(script)") != JSValue.init(undefinedIn: context) {
-				return jsEval(script: "Math.\(script)", context: context)
-			} else {
-				message = "undefined"
+			str = script[index..<script.endIndex]
+		}
+		var charArr: [Character] = []
+		for char in str.characters {
+			charArr.append(char)
+		}
+		for i in 0..<charArr.count {
+			if charArr[i] == "=" {
+				str = str[str.startIndex..<str.index(str.startIndex, offsetBy: i - 1)]
+				if context.evaluateScript(str) != JSValue.init(undefinedIn: context) {
+					return str
+				}
 			}
+		}
+	}
+	return nil
+}
+
+func jsEval(script: String, context: JSContext) -> (eval: JSValue, msg: String) {
+	let evaluated = context.evaluateScript(script)
+	var message: String = "undefined"
+	if let varName = getVariableName(script: script, context: context) {
+		message = "\(varName) = \(context.evaluateScript(varName)!)"
+	} else if evaluated == JSValue.init(undefinedIn: context) {
+		if context.evaluateScript("Math.\(script)") != JSValue.init(undefinedIn: context) {
+			return jsEval(script: "Math.\(script)", context: context)
 		}
 	} else {
 		message = "\(evaluated!)"
