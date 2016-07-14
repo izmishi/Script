5b06de0d28a8caa5cb19b77f80da9a82ffc94724
Izumu Mishima <izmishi11@gmail.com>
2016-07-14 18:00:33 +0100

    iOS Version: 	improved messages when using semicolon for multiple statements on one line

--- jsEval.swift
+++ jsEval.swift
@@ -26,15 +26,46 @@ func getVariableName(script: String, context: JSContext) -> String? {
 				if context.evaluateScript(str) != JSValue.init(undefinedIn: context) {
 					return str.replacingOccurrences(of: " ", with: "")
 				}
+				break
 			}
 		}
 	}
 	return nil
 }
 
-func jsEval(script: String, context: JSContext) -> (eval: JSValue, msg: String) {
-	let evaluated = context.evaluateScript(script)
+func jsEval(script: String, context: JSContext) -> (eval: [JSValue], msg: String) {
+	var eval: [JSValue] = []
 	var message: String = "undefined"
+	var script = script.trimmingCharacters(in: NSCharacterSet.whitespacesAndNewlines())
+	if script == "" {
+		return ([JSValue.init(nullIn: context)], "")
+	}
+	if script.contains(";") && !script.hasPrefix(";") {
+		while script.hasSuffix(";") {
+			print(script)
+			script = script[script.startIndex..<script.index(script.endIndex, offsetBy: -1)]
+		}
+		var semicolonIndices: [Int] = []
+		var charArr: [Character] = []
+		for char in script.characters {
+			charArr.append(char)
+		}
+		for i in 0..<charArr.count {
+			if charArr[i] == ";" {
+				semicolonIndices.append(i + 1)
+			}
+		}
+		message = ""
+		for j in 0...semicolonIndices.count {
+			let start = script.index(script.startIndex, offsetBy: j == 0 ? 0 : semicolonIndices[j - 1])
+			let end = script.index(script.startIndex, offsetBy: j < semicolonIndices.count ?  semicolonIndices[j] : script.characters.count)
+			var (e, m) = jsEval(script: script[start..<end].replacingOccurrences(of: ";", with: ""), context: context)
+			eval.append(e[0])
+			message += "\(m)" + (j < semicolonIndices.count ? "\n" : "")
+		}
+		return (eval, message)
+	}
+	let evaluated = context.evaluateScript(script)
 	if let varName = getVariableName(script: script, context: context) {
 		message = "\(varName) = \(context.evaluateScript(varName)!)"
 	} else if evaluated == JSValue.init(undefinedIn: context) {
@@ -44,5 +75,5 @@ func jsEval(script: String, context: JSContext) -> (eval: JSValue, msg: String) 
 	} else {
 		message = "\(evaluated!)"
 	}
-	return (evaluated!, message)
+	return ([evaluated!], message)
 }
